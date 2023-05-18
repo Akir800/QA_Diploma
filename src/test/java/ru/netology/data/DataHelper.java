f97db1fbcde651baa4c4d4014553efd7f548c08d
Alex Kirillov <akir800@gmail.com>
2023-05-18 22:17:51 +0200

    change test

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -2,58 +2,42 @@ package ru.netology.data;
 
 import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DataHelper {
-    private static final String datasource = System.getProperty("datasource");
+
+    private static final QueryRunner runner = new QueryRunner();
 
     @SneakyThrows
-    public static void databaseCleanUp() {
-        var runner = new QueryRunner();
-        var deleteFromOrder = "DELETE FROM order_entity;";
-        var deleteFromCredit = "DELETE FROM credit_request_entity;";
-        var deleteFromPayment = "DELETE FROM payment_entity;";
+    private static Connection getConn() {
+        String url = System.getProperty("app");
+        return DriverManager.getConnection(url, "app", "pass");
+    }
 
-        try (var connection = DriverManager.getConnection(
-                datasource, "adm", "9mRE")) {
-            runner.update(connection, deleteFromOrder);
-            runner.update(connection, deleteFromCredit);
-            runner.update(connection, deleteFromPayment);
+    @SneakyThrows
+    public static void cleanTables() {
+        var connection = getConn();
+        runner.execute(connection, "DELETE FROM credit_request_entity");
+        runner.execute(connection, "DELETE FROM payment_entity");
+        runner.execute(connection, "DELETE FROM order_entity");
+    }
+
+    @SneakyThrows
+    public static String getPaymentStatus() {
+        var status = "SELECT status FROM payment_entity";
+        try (var conn = getConn()) {
+            return runner.query(conn, status, new ScalarHandler<>());
         }
     }
 
     @SneakyThrows
-    public static CreditRequestEntityInfo getCreditRequestInfo() {
-        var runner = new QueryRunner();
-        var creditRequestInfo = "SELECT * FROM credit_request_entity WHERE created = (SELECT MAX(created) FROM credit_request_entity);";
-
-        try (var connection = DriverManager.getConnection(
-                datasource, "adm", "9mRE")) {
-            return runner.query(connection, creditRequestInfo, new BeanHandler<>(CreditRequestEntityInfo.class));
-        }
-    }
-
-    @SneakyThrows
-    public static PaymentEntityInfo getPaymentInfo() {
-        var runner = new QueryRunner();
-        var paymentInfo = "SELECT * FROM payment_entity WHERE created = (SELECT MAX(created) FROM payment_entity);";
-
-        try (var connection = DriverManager.getConnection(
-                datasource, "adm", "9mRE")) {
-            return runner.query(connection, paymentInfo, new BeanHandler<>(PaymentEntityInfo.class));
-        }
-    }
-
-    @SneakyThrows
-    public static OrderEntityInfo getOrderInfo() {
-        var runner = new QueryRunner();
-        var orderInfo = "SELECT * FROM order_entity WHERE created = (SELECT MAX(created) FROM order_entity);";
-
-        try (var connection = DriverManager.getConnection(
-                datasource, "adm", "9mRE")) {
-            return runner.query(connection, orderInfo, new BeanHandler<>(OrderEntityInfo.class));
+    public static String getCreditStatus() {
+        var status = "SELECT status FROM credit_request_entity";
+        try (var conn = getConn()) {
+            return runner.query(conn, status, new ScalarHandler<>());
         }
     }
 }
---
+++ src/test/java/ru/netology/test/OfferPageDebitTest.java
@@ -0,0 +1,369 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import org.junit.jupiter.api.Test;
+import ru.netology.page.ByWithDebitCard;
+import ru.netology.page.OfferPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.getPaymentStatus;
+import static ru.netology.data.DataGenerator.*;
+
+ public class OfferPageDebitTest {
+
+     @BeforeAll
+     static void setUpAll() {
+         SelenideLogger.addListener("allure", new AllureSelenide());
+     }
+
+     @BeforeEach
+     void setup() {
+         open("http://localhost:8080");
+     }
+
+     @AfterEach
+     void teardown() {
+         DataHelper.cleanTables();
+     }
+
+     @AfterAll
+     static void tearDownAll() {
+         SelenideLogger.removeListener("allure");
+     }
+
+     @Test
+     public void shouldAcceptPurchaseWithApprovedCard() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.waitForNotificationOK();
+         assertEquals("APPROVED", getPaymentStatus());
+     }
+
+     @Test
+     public void shouldDenyPurchaseWithDeclinedCard() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getDeclinedCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.waitForNotificationERROR();
+         assertEquals("DECLINED", getPaymentStatus());
+     }
+
+     @Test
+     public void shouldBeErrorsAfterSendingEmptyRequest() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(null, null, null, null, null);
+         buy.getCardNumberError("Неверный формат");
+         buy.getMonthError("Неверный формат");
+         buy.getYearError("Неверный формат");
+         buy.getCardholderError("Поле обязательно для заполнения");
+         buy.getCVCError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithoutCardNumber() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(null, getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.findMonthError();
+         buy.findYearError();
+         buy.findCardHolderError();
+         buy.findCVCError();
+         buy.getCardNumberError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithoutMonth() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), null, getYear(), getNameOfCardholder(), getCVC());
+         buy.findCardNumberError();
+         buy.findYearError();
+         buy.findCardHolderError();
+         buy.findCVCError();
+         buy.getMonthError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithoutYear() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), null, getNameOfCardholder(), getCVC());
+         buy.findCardNumberError();
+         buy.findMonthError();
+         buy.findCardHolderError();
+         buy.findCVCError();
+         buy.getYearError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithoutCardHolder() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), null, getCVC());
+         buy.findCardNumberError();
+         buy.findMonthError();
+         buy.findYearError();
+         buy.findCVCError();
+         buy.getCardholderError("Поле обязательно для заполнения");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithoutCVC() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getNameOfCardholder(), null);
+         buy.findCardNumberError();
+         buy.findMonthError();
+         buy.findYearError();
+         buy.findCardHolderError();
+         buy.getCVCError("Неверный формат");
+     }
+
+     @Test
+     public void shouldNotAcceptLettersInCardNumberField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(getNameOfCardholder(), null, null, null, null);
+         assertEquals("", buy.getValueFromCardNumber());
+     }
+
+     @Test
+     public void shouldNotAcceptSymbolsInCardNumberField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(getTextOfSymbols(), null, null, null, null);
+         assertEquals("", buy.getValueFromCardNumber());
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithShortCardNumber() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getShortCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.findMonthError();
+         buy.findYearError();
+         buy.findCardHolderError();
+         buy.findCVCError();
+         buy.getCardNumberError("Неверный формат");
+     }
+
+     @Test
+     public void shouldFillFieldCardNumberMax16Digits() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(getLongCardNumber(), null, null, null, null);
+         assertEquals("4444 4444 4444 4441", buy.getValueFromCardNumber());
+     }
+
+     @Test
+     public void shouldBeErrorNotificationAfterSendingRequestWithRandomCardNumber() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getRandomCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.waitForNotificationERROR();
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithInvalidCardNumber() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getInvalidCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVC());
+         buy.findMonthError();
+         buy.findYearError();
+         buy.findCardHolderError();
+         buy.findCVCError();
+         buy.getCardNumberError("Неверный формат");
+     }
+
+     @Test
+     public void shouldNotAcceptLettersInMonthField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, getNameOfCardholder(), null, null, null);
+         assertEquals("", buy.getValueFromMonth());
+     }
+
+     @Test
+     public void shouldNotAcceptSymbolsInMonthField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, getTextOfSymbols(), null, null, null);
+         assertEquals("", buy.getValueFromMonth());
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithInvalidMothBelow() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getInvalidMonthBelow(), getYear(), getNameOfCardholder(), getCVC());
+         buy.getMonthError("Неверно указан срок действия карты");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithInvalidMothAbove() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getInvalidMonthAbove(), getYear(), getNameOfCardholder(), getCVC());
+         buy.getMonthError("Неверно указан срок действия карты");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithInvalidMothOf1Digit() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getInvalidMonthOf1Symbol(), getYear(), getNameOfCardholder(), getCVC());
+         buy.getMonthError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithPreviousDate() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonthFromPreviousDate(), getYearFromPreviousDate(), getNameOfCardholder(), getCVC());
+         buy.getMonthError("Неверно указан срок действия карты");
+     }
+
+     @Test
+     public void shouldNotAcceptLettersInYearField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, null, getNameOfCardholder(), null, null);
+         assertEquals("", buy.getValueFromYear());
+     }
+
+     @Test
+     public void shouldNotAcceptSymbolsInYearField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, null, getTextOfSymbols(), null, null);
+         assertEquals("", buy.getValueFromYear());
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithPreviousYear() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getPreviousYear(), getNameOfCardholder(), getCVC());
+         buy.getYearError("Истёк срок действия карты");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithNextYear() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getNextYear(), getNameOfCardholder(), getCVC());
+         buy.getYearError("Неверно указан срок действия карты");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithDigitsInCardholderField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getCVC(), getCVC());
+         buy.getCardholderError("Поле должно содержать латинские буквы, допустимы дефис и пробел");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithSymbolsInCardholderField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getTextOfSymbols(), getCVC());
+         buy.getCardholderError("Поле должно содержать латинские буквы, допустимы дефис и пробел");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithTextInRussianInCardholderField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getTextInRussian(), getCVC());
+         buy.getCardholderError("Поле должно содержать латинские буквы, допустимы дефис и пробел");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithShortNameInCardholderField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getShortName(), getCVC());
+         buy.getCardholderError("Введите данные в диапазоне от 4 до 60 символов");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithLongNameInCardholderField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getLongName(), getCVC());
+         buy.getCardholderError("Введите данные в диапазоне от 4 до 60 символов");
+     }
+
+     @Test
+     public void shouldNotAcceptLettersInCVCField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, null, null, null, getNameOfCardholder());
+         assertEquals("", buy.getValueFromCVC());
+     }
+
+     @Test
+     public void shouldNotAcceptSymbolsInCVCField() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFields(null, null, null, null, getTextOfSymbols());
+         assertEquals("", buy.getValueFromCVC());
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithCVCOf1Digit() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVCOf1Digit());
+         buy.getCVCError("Неверный формат");
+     }
+
+     @Test
+     public void shouldBeErrorAfterSendingRequestWithCVCOf2Digits() {
+         OfferPage offerPage = new OfferPage();
+         offerPage.openByWithDebitCard();
+         var buy = new ByWithDebitCard();
+         buy.shouldFillFieldsAndSendRequest(getApprovedCardNumber(), getMonth(), getYear(), getNameOfCardholder(), getCVCOf2Digits());
+         buy.getCVCError("Неверный формат");
+     }
+ }
+
